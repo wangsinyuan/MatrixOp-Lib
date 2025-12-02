e9b44962c89da34058db4557831cf5ca31bbe9d0
wangsinyuan <41475904h@gapps.ntnu.edu.tw>
2025-12-02 20:43:39 +0800

    Implement matrix addition and subtraction and element-wise multiplication functions

--- matrixOp.c
+++ matrixOp.c
@@ -1,5 +1,43 @@
-#include<stdio.h>
-int N=3;
-void mul(int A[N][N],int B[N][N],int C[N][N]){
+#include <stdio.h>
+#include "matrix.h"
+
+int i,j;
+
+void add(int A[3][3], int B[3][3], int C[3][3]) 
+{
+    for (int i = 0; i < 3; i++)
+      {
+        for (int j = 0; j < 3; j++)
+          {
+            C[i][j] = A[i][j] + B[i][j];
+          }
+      } 
+}
+
+void sub(int A[3][3], int B[3][3], int C[3][3]) 
+{
+    for (int i = 0; i < 3; i++)
+      {
+        for (int j = 0; j < 3; j++)
+          {
+            C[i][j] = A[i][j] - B[i][j];
+          }
+      } 
+}
+
+void sub(int A[3][3], int B[3][3], int C[3][3]) 
+{
+    for (int i = 0; i < 3; i++)
+      {
+        for (int j = 0; j < 3; j++)
+          {
+            C[i][j] = A[i][j] * B[i][j];
+          }
+      } 
+}
+
+void mul(int A[3][3],int B[3][3],int C[3][3])
+{
   
 }
+
