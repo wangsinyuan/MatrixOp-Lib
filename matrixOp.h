b9654842e12ae03301dab56ea49264d3816e84ee
wangsinyuan <41475904h@gapps.ntnu.edu.tw>
2025-12-02 16:44:03 +0800

    Update matrixOp.h

--- matrixOp.h
+++ matrixOp.h
@@ -1,14 +1,13 @@
-#ifndef MATRIX_OP_H
+#ifndef MATRIX_H
+#define MATRIX_H
 
-#define MATRIX_OP_H 
-
-int* add(int *A,int *B,int size);
-int* sub(int *A,int *B,int size);
-int* ele(int *A,int *B,int size);
-int* det(int *A,int *B,int size);
-int* adj(int *A,int *B,int size);
-int* mat(int *A,int *B,int size);
-int* tra(int *A,int *B,int size);
-int* inv(int *A,int *B,int size);
+int add(int A[3][3], int B[3][3], int result[3][3]);
+int sub(int A[3][3], int B[3][3], int result[3][3]);
+int ele(int A[3][3], int B[3][3], int result[3][3]);
+int det(int A[3][3], int B[3][3], int result[3][3]);
+int adj(int A[3][3], int B[3][3], int result[3][3]);
+int mat(int A[3][3], int B[3][3], int result[3][3]);
+int tra(int A[3][3], int B[3][3], int result[3][3]);
+int inv(int A[3][3], int B[3][3], int result[3][3]);
 
 #endif
