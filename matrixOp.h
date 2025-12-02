89702f04efdea1e4b70a696f935dfbcd324e4262
wangsinyuan <41475904h@gapps.ntnu.edu.tw>
2025-12-02 16:29:10 +0800

    Update matrixOp.h

---
+++ matrixOp.h
@@ -0,0 +1,14 @@
+#ifndef MATRIX_OP_H
+
+#define MATRIX_OP_H 
+
+int* add(int *A,int *B,int size);
+int* sub(int *A,int *B,int size);
+int* ele(int *A,int *B,int size);
+int* det(int *A,int *B,int size);
+int* adj(int *A,int *B,int size);
+int* mat(int *A,int *B,int size);
+int* tra(int *A,int *B,int size);
+int* inv(int *A,int *B,int size);
+
+#endif
