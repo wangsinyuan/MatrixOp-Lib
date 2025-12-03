0e1a493c70d9e454939733f4175527b63062d3a7
Abby3990 <41405002E@gapps.ntnu.edu.tw>
2025-12-03 19:16:36 +0800

    Update matrixOp.c

--- matrixOp.c
+++ matrixOp.c
@@ -36,7 +36,15 @@ void eleMul(int A[3][3], int B[3][3], int C[3][3])
       } 
 }
 
-void mul(int A[3][3],int B[3][3],int C[3][3])
-{
-  
+void mul(int A[3][3], int B[3][3], int C[3][3]) {
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            C[i][j] = 0;
+            for(int k = 0; k < 3; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
 }
+
+
