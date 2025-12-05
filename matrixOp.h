26639a89c246048e3dd03ea3dccdf175d4665ac7
wangsinyuan <41475904h@gapps.ntnu.edu.tw>
2025-12-05 15:37:33 +0800

    matrixOp.h

--- matrixOp.h
+++ matrixOp.h
@@ -4,12 +4,13 @@
 void add(int A[3][3], int B[3][3], int C[3][3]);
 void sub(int A[3][3], int B[3][3], int C[3][3]);
 void eleMul(int A[3][3], int B[3][3], int C[3][3]);
-int det(int A[3][3]);
-void adj(int A[3][3], int B[3][3], int C[3][3]);
 void mul(int A[3][3], int B[3][3], int C[3][3]);
+int det(int A[3][3]);
+void adj(int A[3][3], int C[3][3]);
 void tra(int A[3][3], int C[3][3]);
 int inv(int A[3][3], float C[3][3]);
 
 #endif
+
 
 
