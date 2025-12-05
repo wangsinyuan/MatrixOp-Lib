3e4a14fb72507c0de73fc1724639992c5266b7b6
L5765 <41405018E@gapps.ntnu.edu.tw>
2025-12-05 15:01:57 +0800

    Implement determinant and adjoint functions
    Added determinant and adjoint functions for matrix operations.

--- matrixOp.c
+++ matrixOp.c
@@ -47,6 +47,52 @@ void mul(int A[3][3], int B[3][3], int C[3][3]) {
     }
 }
 
+int det(int A[3][3], int B[3][3], int result[3][3]) {
+    int a11 = A[0][0], a12 = A[0][1], a13 = A[0][2];
+    int a21 = A[1][0], a22 = A[1][1], a23 = A[1][2];
+    int a31 = A[2][0], a32 = A[2][1], a33 = A[2][2];
+
+    int d =
+        a11 * a22 * a33 +
+        a12 * a23 * a31 +
+        a13 * a21 * a32 -
+        a13 * a22 * a31 -
+        a11 * a23 * a32 -
+        a12 * a21 * a33;
+
+    result[0][0] = d;
+    result[0][1] = result[0][2] =
+    result[1][0] = result[1][1] = result[1][2] =
+    result[2][0] = result[2][1] = result[2][2] = 0;
+
+    return 0;
+}
+
+int adj(int A[3][3], int B[3][3], int result[3][3]) {
+    int a11 = A[0][0], a12 = A[0][1], a13 = A[0][2];
+    int a21 = A[1][0], a22 = A[1][1], a23 = A[1][2];
+    int a31 = A[2][0], a32 = A[2][1], a33 = A[2][2];
+
+    int c11 =  (a22 * a33 - a23 * a32);
+    int c12 = -(a21 * a33 - a23 * a31);
+    int c13 =  (a21 * a32 - a22 * a31);
+
+    int c21 = -(a12 * a33 - a13 * a32);
+    int c22 =  (a11 * a33 - a13 * a31);
+    int c23 = -(a11 * a32 - a12 * a31);
+
+    int c31 =  (a12 * a23 - a13 * a22);
+    int c32 = -(a11 * a23 - a13 * a21);
+    int c33 =  (a11 * a22 - a12 * a21);
+
+    result[0][0] = c11;  result[0][1] = c21;  result[0][2] = c31;
+    result[1][0] = c12;  result[1][1] = c22;  result[1][2] = c32;
+    result[2][0] = c13;  result[2][1] = c23;  result[2][2] = c33;
+
+    return 0;
+}
+
+
 void tra(int A[3][3], int C[3][3]) {
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
@@ -69,3 +115,4 @@ int inv(int A[3][3], float C[3][3]){
         }
     return 1;
 }
+
