2831e3fb8df1099c017b48dc21db8197496976dd
wangsinyuan <41475904h@gapps.ntnu.edu.tw>
2025-12-05 15:50:38 +0800

    Refactor main function for matrix operations

--- main.c
+++ main.c
@@ -1,115 +1,95 @@
 #include <stdio.h>
 #include "matrixOp.h"
 
-int main()
-{
-  int A[3][3]={{1,2,3},{4,5,6},{7,8,10}};
-  int B[3][3]={{2,1,3},{1,2,1},{3,1,2}};
-  int C[3][3];
-  int i,j;
-  
-  printf("Matrix A:\n"); 
-  for(i=0;i<3;i++)
-  {
-        for(j=0;j<3;j++)
-          {
+int main() {
+    int A[3][3] = {{1,2,3},{4,5,6},{7,8,10}};
+    int B[3][3] = {{2,1,3},{1,2,1},{3,1,2}};
+    int C[3][3];
+    float C_inv[3][3];
+
+    printf("Matrix A:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
             printf("%d ", A[i][j]);
-          }
+        }
         printf("\n");
     }
-  
-  printf("Matrix B:\n"); 
-  for(i=0;i<3;i++)
-  {
-        for(j=0;j<3;j++)
-          {
+
+    printf("\nMatrix B:\n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
             printf("%d ", B[i][j]);
-          }
-        printf("\n");
-    }
-  
-  printf("\nAddition:\n");
-  add(A, B, C);
-  for(i=0;i<3;i++)
-  {
-        for(j=0;j<3;j++)
-          {
-            printf("%d ", C[i][j]);
-          }
+        }
         printf("\n");
     }
 
-  printf("\nSubtraction:\n");
-  sub(A, B, C);
-  for(i=0;i<3;i++)
-  {
-        for(j=0;j<3;j++)
-          {
+    printf("\nAddition:\n");
+    add(A,B,C);
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
             printf("%d ", C[i][j]);
-          }
-        printf("\n");
-    }
-  
-  printf("\nElement-wise Multiplication:\n");
-  eleMul(A, B, C);
-  for(i=0;i<3;i++)
-  {
-        for(j=0;j<3;j++)
-          {
-            printf("%d ", C[i][j]);
-          }
+        }
         printf("\n");
     }
 
-   printf("\nMatrix Multiplication A*B:\n");
-   mul(A,B,C);
-   for(i=0;i<3;i++)
-  {
-        for(j=0;j<3;j++)
-          {
+    printf("\nSubtraction:\n");
+    sub(A,B,C);
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
             printf("%d ", C[i][j]);
-          }
+        }
         printf("\n");
     }
 
-   printf("\nTranspose of A:\n");
-   tra(A,C);
-   for(i=0;i<3;i++)
-  {
-        for(j=0;j<3;j++)
-          {
+    printf("\nElement-wise Multiplication:\n");
+    eleMul(A,B,C);
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
             printf("%d ", C[i][j]);
-          }
+        }
         printf("\n");
     }
 
-  printf("\nDeterminant of A: %d\n", det(A));
-
-  printf("\nAdjoint of A:\n");
-  adj(A,C);
-  for(i=0;i<3;i++)
-  {
-        for(j=0;j<3;j++)
-          {
+    printf("\nMatrix Multiplication A*B:\n");
+    mul(A,B,C);
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
             printf("%d ", C[i][j]);
-          }
+        }
         printf("\n");
     }
 
-  if(inv(A,C_inv)){
-        printf("\nInverse of A:\n");
-        for(i=0;i<3;i++)
-        {
-          for(j=0;j<3;j++)
-            {
-              printf("%.2f ", C[i][j]);
+    printf("\nTranspose of A:\n");
+    tra(A,C);
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nDeterminant of A: %d\n", det(A));
+
+    printf("\nAdjoint of A:\n");
+    adj(A,C);
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nInverse of A:\n");
+    if(inv(A, C_inv)){
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                printf("%.2f ", C_inv[i][j]);
             }
-          printf("\n");
-         }
-    } 
-  else {
-        printf("\nA is not invertible.\n");
+            printf("\n");
+        }
+    } else {
+        printf("A is not invertible.\n");
     }
 
-  return 0;
+    return 0;
 }
